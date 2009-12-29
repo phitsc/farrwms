c9b031b912aa67841c8c9991bbfd65c6afdaca99
phitsc <phitsc@localhost>
2009-12-29 21:12:33 +0000

    added deploy.bat

---
+++ Deploy/_Deploy.bat
@@ -0,0 +1,2 @@
+REM C:\Projects\UpdateVersion\Deploy\UpdateVersion.exe -s c:\Projects\FarrMilk\Deploy\FarrMilk.dll -t c:\Projects\FarrMilk\Deploy\FarrMilk.dcupdate -t c:\Projects\FarrMilk\Deploy\versioninfo.xml
+"C:\Program Files\7-Zip\7z.exe" a FarrWebMetaSearch.zip C:\Users\Philipp\Projekte\FarrWebMetaSearch\Deploy\*.* -r -xr!.svn -x!versioninfo.xml -x!_Deploy.bat -x!*.zip -tzip
